9257a4d441bf3fcaa383a63ea0b54d5d4367daae
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-11 10:00:48 +0100

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
-    <div className="min-h-screen w-full px-2 py-5 dark:bg-gray-900">
-      <div className="container mx-auto">
-        <header className="flex items-center">
-          <h1 className="text-gray-900 dark:text-gray-200 text-xl font-bold">Porfolio</h1>
+    <div className='min-h-screen w-full px-2 py-5 dark:bg-gray-900'>
+      <div className='container mx-auto'>
+        <header className='flex items-center'>
+          <h1 className='text-xl font-bold text-gray-900 dark:text-gray-200'>
+            Porfolio
+          </h1>
         </header>
       </div>
     </div>
-  );
+  )
 }
