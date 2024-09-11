eae5eeccf41cb24c5e793b741d0a50ab19a9f00c
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-11 10:08:39 +0100

    feat: shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className='min-h-screen w-full px-2 py-5 dark:bg-gray-900'>
+    <div className='min-h-screen w-full px-2 py-5'>
       <div className='container mx-auto'>
         <header className='flex items-center'>
-          <h1 className='text-xl font-bold text-gray-900 dark:text-gray-200'>
+          <h1 className='text-xl font-bold'>
             Porfolio
           </h1>
         </header>
