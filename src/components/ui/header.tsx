6f7d13308f541d182b63fc019b10a94a0144d28d
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-18 12:07:22 +0100

    feat: header translate links

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,26 +2,20 @@ import React from 'react'
 import ThemeSwitcher from './theme-switcher'
 import Link from 'next/link'
 import LanguageSelector from './language-selector'
+import { useMessages } from 'next-intl'
 
-const links = [
-  {
-    label: 'Sobre',
-    link: '/about'
-  },
-  {
-    label: 'Projectos',
-    link: '/projects'
-  },
-  {
-    label: 'Artigos',
-    link: '/articles'
-  },
-  {
-    label: 'Contacto',
-    link: '/contact'
-  }
-]
+type LayoutProps = {
+  header: {
+    label: string;
+    link: string;
+  }[]
+}
+
 export default function Header() {
+  const messages = useMessages();
+  const layout: unknown = messages.layout;
+  const {header} = layout as LayoutProps;
+
   return (
     <header className='flex items-center justify-between px-4 fixed z-50 inset-x-0 w-full top-0 backdrop-blur-sm'>
       <div className='container flex items-center justify-between py-4'>
@@ -30,8 +24,8 @@ export default function Header() {
         </h1>
         <nav className='flex items-center gap-5 md:gap-10'>
           <ul className='hidden md:flex items-center gap-5 text-gray-700 dark:text-gray-200'>
-            {links.map((item) => (
-              <li key={item.link} className='text-xs md:text-base'>
+            {header?.map((item) => (
+              <li key={item.label} className='text-xs md:text-base'>
                 <Link href={item.link}>{item.label}</Link>
               </li>
             ))}
