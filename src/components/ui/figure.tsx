0602deb5caf280312e78dfb3e3ac577236f73e1f
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-19 11:15:49 +0100

    refactor: lazy hook

--- src/components/ui/figure.tsx
+++ src/components/ui/figure.tsx
@@ -1,39 +1,22 @@
 "use client"
+import { useLazy } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
-import Image, { ImageProps } from "next/image";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import NextImage from "next/image";
+import { ImageProps } from "next/image";
+import React, { useRef } from "react";
 
 type Props = ImageProps & {
     figureClass?: ClassValue;
 }
 
 export const LazyFigure = (({className, figureClass,alt,...rest}:Props) =>{
-    const [visible,setVisibe] = useState(false);
-    const figureRef = useRef<HTMLImageElement | null>(null);
-
-    const loadImage = useCallback(()=>{
-        const image = document.createElement("img")
-        image.src = rest.src as string;
-        image.onload = () => setVisibe(true);
-    },[rest.src])
-
-    useEffect(()=>{
-        const observer = new IntersectionObserver((observers)=>{
-            const myImage = observers[0];
-            if(myImage.isIntersecting)
-            loadImage()
-        })
-        
-        if(figureRef.current){
-            observer.observe(figureRef.current);
-        }
-        return () => observer.disconnect()
-    },[figureRef, loadImage])
+    const elementRef = useRef<HTMLImageElement | null>(null);
+    const {visible,isPending} = useLazy({elementRef})
 
     return (
-        <figure ref={figureRef} className={cn("relative",figureClass)}>
-            {visible ? <Image className={cn("size-full",className)} {...rest} alt={alt}/>: (
+        <figure ref={elementRef} className={cn("relative",figureClass)}>
+            {(visible && !isPending) ? <NextImage className={cn("size-full",className)} {...rest} alt={alt}/>: (
                 <div className="absolute inset-0 figure"/>
             )}
         </figure>
