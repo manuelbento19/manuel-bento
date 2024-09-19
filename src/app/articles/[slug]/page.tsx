44fc499a2560320473a7464ebac98e2cdf549dbc
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-19 16:20:10 +0100

    feat: tailwind typography

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -30,14 +30,16 @@ export default function Page({ params }: { params: { slug: string } }){
             <header>
                 <Link href="/articles" className='flex items-center gap-2'><ArrowLeftIcon className="size-4"/> Voltar</Link>
             </header>
-            <section className="pb-6 space-y-4">
+            <section className="pb-6 space-y-4 text-zinc-900 dark:text-zinc-200">
                 <header className="contents py-4">
-                    <h1 className="text-lg md:text-4xl text-zinc-900 dark:text-zinc-200">{article.title}</h1>
+                    <h1 className="text-lg md:text-4xl">{article.title}</h1>
                     <time dateTime={article.date} title={article.date} className="text-xs text-neutral-700 dark:text-zinc-400">
                         {formatDate(article.date)}
                     </time>
                 </header>
-                <MdxProvider content={article.body.code} />
+                <div className="prose dark:prose-invert max-w-full">
+                    <MdxProvider content={article.body.code} />
+                </div>
                 <footer className="inline-flex gap-2 mt-4">
                     {article.tags.map((tag) => (
                         <Tag key={tag} className="text-xs rounded-md">#{tag}</Tag>
