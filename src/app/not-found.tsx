fd142fa669708f0a88fbd95f23ab7a60f17fb485
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-19 01:42:49 +0100

    feat: drop shadow

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function Notfound() {
     <div className='h-full bg-[--background]'>
       <div className='h-full container grid place-content-center'>
         <div className="space-y-6 text-center">
-          <Image src={image}  alt='404 page' width={300} height={300} loading='lazy' className='max-w-md w-full'/>
+          <Image src={image}  alt='404 page' width={300} height={300} loading='lazy' className='max-w-md w-full drop-shadow-[0_5px_5px_rgba(0,0,0,.5)] dark:drop-shadow-none'/>
           <h1 className='font-bold text-xl md:text-3xl'>Porquê que estás aqui?</h1>
           <h2 className='font-medium text-sm md:text-base'>Você não deveria estar aqui.</h2>
           <Button size="lg" asChild>
