9f099c482c01d470b3c09618c0fba0ae1717de3e
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-30 21:28:31 +0100

    feat: order post date

--- src/app/articles/_partials/list.tsx
+++ src/app/articles/_partials/list.tsx
@@ -5,7 +5,7 @@ import { allArticles } from 'contentlayer/generated'
 export default function ArticleList() {
   return (
     <section className='grid grid-cols-1 gap-4 py-2 md:grid-cols-2'>
-      {allArticles.reverse().map((item) => (
+      {allArticles.sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf()).map((item) => (
         <ArticleCard key={item.title} article={item} />
       ))}
     </section>
